c69a3f4fbfb9e0ea659642fd0c498265d256b73a
FaletehanAlF <faletehanalfarabi09>
2026-08-28 11:02:08 +0700

    feat: add grade calculation logic

--- grade-calculator/main.go
+++ grade-calculator/main.go
@@ -8,5 +8,15 @@ func main() {
 	fmt.Print("Masukkan nilai: ")
 	fmt.Scanln(&grade)
 
-	fmt.Println("Nilai yang kamu masukkan:", grade)
+	if grade >= 90 {
+		fmt.Println("Grade: A")
+	} else if grade >= 80 {
+		fmt.Println("Grade: B")
+	} else if grade >= 70 {
+		fmt.Println("Grade: C")
+	} else if grade >= 60 {
+		fmt.Println("Grade: D")
+	} else {
+		fmt.Println("Grade: E")
+	}
 }

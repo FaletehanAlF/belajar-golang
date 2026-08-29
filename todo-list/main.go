9b28593b9458e896bcfa9ef11e073d3c99f31107
FaletehanAlF <faletehanalfarabi09>
2026-08-29 11:00:43 +0700

    feat: add main menu

---
+++ todo-list/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=== TO-DO LIST ===")
+	fmt.Println("1. Tambah Tugas")
+	fmt.Println("2. Lihat Tugas")
+	fmt.Println("3. Hapus Tugas")
+	fmt.Println("4. Keluar")
+}
